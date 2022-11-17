4d3ebd431c0b217d8cdadb2b28a13c1ac562d6cd
brandao07 <75815203+brandao07@users.noreply.github.com>
2022-11-17 18:33:15 +0000

    3-php init

---
+++ 3-php/src/back-office/index.php
@@ -0,0 +1,8 @@
+<html lang="en">
+<head>
+    <title>Back-Office</title>
+</head>
+<body>
+<?php echo '<p>Hello Back-Office</p>'; ?>
+</body>
+</html>
---
+++ 3-php/src/front-office/index.php
@@ -0,0 +1,8 @@
+<html lang="en">
+<head>
+    <title>Portfolio</title>
+</head>
+<body>
+<?php echo '<p>Hello Front-Office</p>'; ?>
+</body>
+</html>
