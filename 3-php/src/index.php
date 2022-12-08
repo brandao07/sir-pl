c5bc73dfe89c72a1618e91211090312be0f0f533
brandao07 <75815203+brandao07@users.noreply.github.com>
2022-12-08 20:51:25 +0000

    image needs fix

--- 3-php/src/index.php
+++ 3-php/src/index.php
@@ -1,14 +1,37 @@
 <?php
-require "utils/functions.php";
 require "db/connection.php";
 
+function debug_to_console($data): void
+{
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+
 $pdo = pdo_connect_mysql();
+
+// ME
 $stmt = $pdo->prepare('SELECT * FROM me WHERE id = 1');
 $stmt->execute();
 $me = $stmt->fetch(PDO::FETCH_ASSOC);
-if (!$me) {
-    exit('Portfolio doesn\'t exist with that ID!');
-}
+
+$stmt = $pdo->prepare('SELECT * FROM files WHERE id = ?');
+$stmt->execute([$me["files_id"]]);
+$me_image = $stmt->fetch(PDO::FETCH_ASSOC);
+$me_image = $me_image["file_data"];
+//FIXME: 414 (Request-URI Too Long
+debug_to_console($me_image);
+
+// ABOUT
+$stmt = $pdo->prepare('SELECT * FROM abouts WHERE id_me = 1 AND is_deleted = 0');
+$stmt->execute();
+$abouts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// EDUCATION
+
+//
 ?>
 
 <!DOCTYPE html>
@@ -18,7 +41,7 @@ if (!$me) {
     <meta
             content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
             name="viewport"
-                />
+    />
     <meta content="ie=edge" http-equiv="X-UA-Compatible"/>
     <title>Portfolio</title>
     <!-- Bootstrap CSS -->
@@ -27,26 +50,26 @@ if (!$me) {
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
             integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
             rel="stylesheet"
-                />
+    />
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com" rel="preconnect"/>
     <link crossorigin href="https://fonts.gstatic.com" rel="preconnect"/>
     <link
             href="https://fonts.googleapis.com/css2?family=Merriweather:wght@700&family=Montserrat:wght@400;500;600&family=Poppins:ital@1&display=swap"
             rel="stylesheet"
-                />
+    />
     <!-- Font Awesome -->
     <script
             crossorigin="anonymous"
             src="https://kit.fontawesome.com/3ae97c3eed.js"
-                ></script>
+    ></script>
     <!-- Bootstrap JS -->
     <script
             crossorigin="anonymous"
             defer
             integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-                ></script>
+    ></script>
     <!--External CSS -->
     <link href="styles.css" rel="stylesheet"/>
 </head>
@@ -56,15 +79,15 @@ if (!$me) {
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-auto col-lg-4">
-                <p class="hero-quote"><?= $me["quote"]?></p>
+                <p class="hero-quote"><?= $me["quote"] ?></p>
             </div>
             <div class="col-auto my-auto">
                 <img
-                        alt="andre"
+                        alt=<?= $me["name"] ?>
                         class="hero-img"
-                        src="./assets/me.jpeg"
+                        src=<?= $me_image ?>
                         width="250"
-                            />
+                />
             </div>
         </div>
     </div>
@@ -74,32 +97,17 @@ if (!$me) {
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-auto section-title">
-                <h1>Hi, I'm <?= $me["name"]?> !</h1>
+                <h1>Hi, I'm <?= $me["name"] ?> !</h1>
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-4 col-sm-12">
-                <p>
-                    Computer Science Bachelor Finalist on IPVC-ESTG. I'm thinking
-                    about taking my Master's Degree at Universidade do Minho in Braga.
-                    Right now I'm mostly learning the Go language, and I'm loving it
-                    so far!
-                </p>
-            </div>
-            <div class="col-lg-4 col-sm-12">
-                <p>
-                    Ever since I was little, I had this huge passion for technology
-                    and solving things which may seem complicated to others, thus why
-                    I chose this field.
-                </p>
-            </div>
-            <div class="col-lg-4 col-sm-12">
-                <p>
-                    I find myself in music, it helps me with all my emotions through
-                    the good and hard times. It gives me the creativity and the
-                    motivation that I need.
-                </p>
-            </div>
+            <?php foreach ($abouts as $a): ?>
+                <div class="col-lg-4 col-sm-12">
+                    <p>
+                        <?= $a['description'] ?>
+                    </p>
+                </div>
+            <?php endforeach; ?>
         </div>
     </div>
 </section>
@@ -123,7 +131,7 @@ if (!$me) {
             <div class="col-lg-12">
                 <p>Bachelor's Finalist, taking Computer Science and Engineering.</p>
                 <p>
-During my academic journey, I've found something that I truly love
+                    During my academic journey, I've found something that I truly love
                     and live for.
                 </p>
                 <p>
@@ -144,7 +152,7 @@ During my academic journey, I've found something that I truly love
         <div class="row justify-content-center">
             <div class="col-lg-6">
                 <p>
-                    <?= $me["skills_description"]?>
+                    <?= $me["skills_description"] ?>
                 </p>
             </div>
         </div>
@@ -152,42 +160,42 @@ During my academic journey, I've found something that I truly love
                 class="carousel slide"
                 data-bs-ride="carousel"
                 id="carouselSkills"
-                    >
+        >
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <img
                             alt="typescript"
                             class="d-block skill-img mx-auto"
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
-                                />
+                    />
                 </div>
                 <div class="carousel-item">
                     <img
                             alt="golang"
                             class="d-block skill-img mx-auto"
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg"
-                                />
+                    />
                 </div>
                 <div class="carousel-item">
                     <img
                             alt="java"
                             class="d-block skill-img mx-auto"
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg"
-                                />
+                    />
                 </div>
                 <div class="carousel-item">
                     <img
                             alt="postgresql"
                             class="d-block skill-img mx-auto"
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain-wordmark.svg"
-                                />
+                    />
                 </div>
                 <div class="carousel-item">
                     <img
                             alt="mongodb"
                             class="d-block skill-img mx-auto"
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain-wordmark.svg"
-                                />
+                    />
                 </div>
             </div>
             <button class="carousel-control-prev" data-bs-slide="prev" data-bs-target="#carouselSkills" type="button">
@@ -212,7 +220,7 @@ During my academic journey, I've found something that I truly love
         <div class="row justify-content-center">
             <div class="col-lg-6">
                 <p>
-                    <?= $me["languages_description"]?>
+                    <?= $me["languages_description"] ?>
                 </p>
             </div>
         </div>
@@ -228,7 +236,7 @@ During my academic journey, I've found something that I truly love
     <div class="row justify-content-center">
         <div class="col-lg-6">
             <p>
-                <?= $me["certificates_description"]?>
+                <?= $me["certificates_description"] ?>
             </p>
         </div>
     </div>
@@ -321,45 +329,35 @@ During my academic journey, I've found something that I truly love
                         class="footer-link"
                         href="https://www.last.fm/user/sunkye"
                         target="_blank"
-                >
-                    <i class="fa-brands fa-lastfm footer-icon"></i>
-                </a>
+                >LastFM</a>
             </div>
             <div class="col-auto">
                 <a
                         class="footer-link"
                         href="https://github.com/brandao07"
                         target="_blank"
-                >
-                    <i class="fa-brands fa-github footer-icon"></i>
-                </a>
+                >GitHub</a>
             </div>
             <div class="col-auto">
                 <a
                         class="footer-link"
                         href="https://www.linkedin.com/in/andre-brandao07/"
                         target="_blank"
-                >
-                    <i class="fa-brands fa-linkedin footer-icon"></i>
-                </a>
+                >LinkedIn</a>
             </div>
             <div class="col-auto">
                 <a
                         class="footer-link"
                         href="https://www.instagram.com/brandao_237/"
                         target="_blank"
-                >
-                    <i class="fa-brands fa-instagram footer-icon"></i>
-                </a>
+                >Instagram</a>
             </div>
             <div class="col-auto">
                 <a
                         class="footer-link"
                         href="https://twitter.com/brandao_237"
                         target="_blank"
-                >
-                    <i class="fa-brands fa-twitter footer-icon"></i>
-                </a>
+                >Twitter</a>
             </div>
         </div>
     </div>
