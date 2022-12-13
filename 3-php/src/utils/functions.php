215090d0dd4587c36f2a730e57cd5aae33bc286c
brandao07 <75815203+brandao07@users.noreply.github.com>
2022-12-13 12:53:57 +0000

    added icons

--- 3-php/src/utils/functions.php
+++ 3-php/src/utils/functions.php
@@ -15,7 +15,8 @@ echo <<<EOT
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>My CMS</title>
+		<title>26244 - CMS</title>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 		<link href="/utils/styles.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
